19cbde821038d1ee05b23fecb5b04e65a6200f58
Dastan-dev <Zhurumbaevdastan@gmail.com>
2019-11-17 17:44:21 +0600

    part2.task10

---
+++ task10.py
@@ -0,0 +1,12 @@
+stroka = input("vvedite slovo: ")
+
+
+print(stroka[2])
+print(stroka[-1])
+print(stroka[:4])
+print(stroka[-2])
+print(stroka[::2])
+print(stroka[1::2])
+print(stroka[::-1])
+print(stroka[-1::-2])
+print(len(stroka))
